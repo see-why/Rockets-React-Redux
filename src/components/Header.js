2e656e1fb80f956a70783ad405d926ffa3265fb2
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-08 13:30:33 +0200

    Create header component and import the NavLink inside

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-const Header = () => {
-    return ( 
-        
-     );
-}
- 
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+// import '../css/app.css';
+import planet from '../img/planet.png';
+
+const Header = () => (
+  <header>
+    <nav>
+      <img src={planet} alt="logo" />
+      <h2>Space Travelers &apos; Hub</h2>
+      <ul>
+        <li>
+          <NavLink to="/">Rocket</NavLink>
+          <NavLink to="./Missions">Missions</NavLink>
+          <NavLink to="./Profile">My profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
 export default Header;
