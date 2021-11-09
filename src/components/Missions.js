289f8b499ff3d9a3c67b42218e0c95e6a1a93cb2
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-09 10:41:38 +0200

    Create Table for the Missions component

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,21 @@
 import React from 'react';
+// import { useSelector } from 'react-redux';
+// import MissionsBody from './MissionsBody';
 
-const Missions = () => (<h2>Mission</h2>);
-
+//   const { missions } = useSelector((state) => state).missions;
+const Missions = () => (
+  <section className="missions-section">
+    <table className="mission-table">
+      <thead>
+        <tr>
+          <th>Missions</th>
+          <th>Descripition</th>
+          <th>Status</th>
+          <th aria-label="Leave/Join Mission" />
+        </tr>
+      </thead>
+      {/* <MissionsBody missions={missions} /> */}
+    </table>
+  </section>
+);
 export default Missions;
