456928546c261321a60610fb39f0943d123cf553
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-09 11:19:41 +0200

    Merge the Rochets component from the dev branch

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-// import store from './app/store';
 import './index.css';
 import './css/app.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
     <Router>
       <Provider store={store}>
         <App />
       </Provider>
     </Router>
-    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root'),
 );
