993fefbae4a4a51ff6938ed03d7447fbe9a98658
Iyadi Cyril <iyadicy@gmail.com>
2021-11-09 22:22:19 +0100

    Add missions and rockets sections

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
-const Profile = () => (<h2>My Profile</h2>);
+const Profile = () => {
+  const { rockets } = useSelector((state) => state);
+
+  const reservedRockets = rockets.filter((item) => (item.reserved === true));
+
+  return (
+    <div className="my-profile">
+      <div className="my-missions">
+        <h2>My Missions</h2>
+      </div>
+      <div className="my-rockets">
+        <h2>My Rockets</h2>
+        <ul className="rocket-Lists">
+          {reservedRockets.map((item) => (<li key={uuidv4()}>{item.name}</li>))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
