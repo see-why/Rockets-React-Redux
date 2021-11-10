06f33225c11a1993f41eb762f9ca744c14b653c0
Iyadi Cyril <iyadicy@gmail.com>
2021-11-10 17:54:11 +0100

    Add test files

---
+++ src/_tests_/header.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { configure, shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Header from '../components/header';
+
+configure({ adapter: new Adapter() });
+
+describe('the UI page', () => {
+  test('displays correctly', () => {
+    const component = shallow(<Header />);
+    expect(component).toMatchSnapshot();
+  });
+
+  test('should have Home, Calculator and Quote link', () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText('Rockets')).toHaveTextContent('Rockets');
+    expect(screen.getByText('Missions')).toHaveTextContent('Missions');
+    expect(screen.getByText('My profile')).toHaveTextContent('My profile');
+  });
+});
