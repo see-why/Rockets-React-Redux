1bbfb56ac0521a46e3385f89419da52cb6abf380
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-10 16:51:47 +0200

    Import missions.module.css file inside the Missions.js file

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import MissionsBody from './MissionsBody';
+import styles from '../css/missions.module.css';
 
 const Missions = () => {
   const { missions } = useSelector((state) => state);
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
+      <table className={styles.table}>
+        <thead className={styles.head}>
+          <tr className={styles.row}>
             <th>Missions</th>
             <th>Description</th>
             <th>Status</th>
